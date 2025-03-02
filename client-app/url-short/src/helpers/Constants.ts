89df8fcb969ecab8211cca435f3d09f778b8117a
DemonSSvz <121257864+demonssvz@users.noreply.github.com>
2025-03-02 23:17:24 +0530

    Update Constants.ts

--- client-app/url-short/src/helpers/Constants.ts
+++ client-app/url-short/src/helpers/Constants.ts
@@ -1,2 +1,2 @@
 export const serverUrl =
-  import.meta.env.VITE_SERVER_URL || "https://url-shortner-obp5.vercel.app";
+  import.meta.env.VITE_SERVER_URL || "https://url-shortner-obp5.vercel.app/api"
