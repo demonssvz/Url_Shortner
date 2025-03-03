20e86891fc91187f9a5e31516f7f4c0452820267
DemonSSvz <121257864+demonssvz@users.noreply.github.com>
2025-03-03 10:07:37 +0530

    Update server.ts

--- server-app/src/server.ts
+++ server-app/src/server.ts
@@ -6,30 +6,25 @@ import shortUrl from "./routes/shortUrl";
 dotenv.config();
 connectDb();
 
-  
- const port = process.env.PORT || 5001;
-
+const port = process.env.PORT || 5001;
 
 const app = express();
-app.listen(port, () => {
-  console.log(`✅ Server started successfully on port: ${port}`);
-});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: ["http://localhost:3000",
-      "https://url-shortner-n86u.vercel.app/api"
-     ] ,
+       "https://url-shortner-n86u.vercel.app"
+      ] ,
     credentials: true,
   })
 );
 
-app.use("/api/", shortUrl);
-
 app.get("/", (req, res) => {
   res.send("Backend is working!");
 });
+
+app.use("/api/", shortUrl);
 
 app.listen(port, () => {
   console.log(`Server started successfully on port : ${port}`);
